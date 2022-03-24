e136a449106c894a78c993eae6e6e86802c4ea15
lscjjj <49670860+lscjjj@users.noreply.github.com>
2022-03-24 09:43:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <a href="https://github.com/drawcall/threejs-mesh-modifiers" target="_blank">
+      <img class="logo" src="./assets/logo.png">
+    </a>
+    <Btns/>
+    <World/>
+  </div>
+</template>
+
+<script>
+import xEmiiter from "xemitterjs";
+import Btns from "./components/Btns";
+import World from "./components/World";
+
+export default {
+  name: "App",
+  components: {
+    Btns,
+    World
+  },
+  methods: {
+    click: function(type) {
+      xEmiiter.emit("MENU_CLICK", type);
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+}
+.logo {
+  width: 60px;
+  position: absolute;
+  z-index: 999;
+  left: 30px;
+  top: 30px;
+  opacity: 0.7;
+}
+</style>
